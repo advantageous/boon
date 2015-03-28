270b6d455d7ca5cb77d441ee9de60f6a4f9f6976
Geoffrey Chandler <geoffc@gmail.com>
2015-03-27 18:29:13 -0700

    refactored boon to its own project

--- qbit-core/src/main/java/io/advantageous/qbit/client/RemoteTCPClientProxy.java
+++
@@ -1,8 +0,0 @@
-package io.advantageous.qbit.client;
-
-public interface RemoteTCPClientProxy extends ClientProxy {
-    default int port() {return 0;}
-    default String host() {return "localProxy";}
-    default boolean connected() {return true;}
-    default boolean remote() {return true;}
-}
